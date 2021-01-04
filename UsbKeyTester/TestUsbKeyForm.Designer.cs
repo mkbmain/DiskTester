380b1bc1a52452f9579bc349a2da1153623dc033
michael.bourke <michael.bourke@capitalontap.com>
2021-01-04 22:11:25 +0000

    Wolla

---
+++ UsbKeyTester/TestUsbKeyForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace UsbKeyTester
+{
+    partial class TestUsbKeyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DriveDropDownBox = new System.Windows.Forms.ComboBox();
+            this.DriveSelectLbl = new System.Windows.Forms.Label();
+            this.RunBtn = new System.Windows.Forms.Button();
+            this.CreateFileProgressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CheckFileProgressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // DriveDropDownBox
+            // 
+            this.DriveDropDownBox.FormattingEnabled = true;
+            this.DriveDropDownBox.Location = new System.Drawing.Point(71, 12);
+            this.DriveDropDownBox.Name = "DriveDropDownBox";
+            this.DriveDropDownBox.Size = new System.Drawing.Size(139, 21);
+            this.DriveDropDownBox.TabIndex = 0;
+            // 
+            // DriveSelectLbl
+            // 
+            this.DriveSelectLbl.Location = new System.Drawing.Point(-2, 15);
+            this.DriveSelectLbl.Name = "DriveSelectLbl";
+            this.DriveSelectLbl.Size = new System.Drawing.Size(67, 23);
+            this.DriveSelectLbl.TabIndex = 1;
+            this.DriveSelectLbl.Text = "Select drive:";
+            // 
+            // RunBtn
+            // 
+            this.RunBtn.Location = new System.Drawing.Point(100, 97);
+            this.RunBtn.Name = "RunBtn";
+            this.RunBtn.Size = new System.Drawing.Size(75, 23);
+            this.RunBtn.TabIndex = 2;
+            this.RunBtn.Text = "Run";
+            this.RunBtn.UseVisualStyleBackColor = true;
+            this.RunBtn.Click += new System.EventHandler(this.RunBtn_Click);
+            // 
+            // CreateFileProgressBar
+            // 
+            this.CreateFileProgressBar.Location = new System.Drawing.Point(71, 39);
+            this.CreateFileProgressBar.Name = "CreateFileProgressBar";
+            this.CreateFileProgressBar.Size = new System.Drawing.Size(139, 23);
+            this.CreateFileProgressBar.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(-2, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Create File";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(-2, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Check file";
+            // 
+            // CheckFileProgressBar
+            // 
+            this.CheckFileProgressBar.Location = new System.Drawing.Point(71, 68);
+            this.CheckFileProgressBar.Name = "CheckFileProgressBar";
+            this.CheckFileProgressBar.Size = new System.Drawing.Size(139, 23);
+            this.CheckFileProgressBar.TabIndex = 5;
+            // 
+            // TestUsbKeyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 129);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CheckFileProgressBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CreateFileProgressBar);
+            this.Controls.Add(this.RunBtn);
+            this.Controls.Add(this.DriveSelectLbl);
+            this.Controls.Add(this.DriveDropDownBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "TestUsbKeyForm";
+            this.Text = "Test Usb Key Form";
+            this.Load += new System.EventHandler(this.TestUsbKeyForm_Load);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.ProgressBar CheckFileProgressBar;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar CreateFileProgressBar;
+
+        private System.Windows.Forms.Button RunBtn;
+
+        private System.Windows.Forms.Label DriveSelectLbl;
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox DriveDropDownBox;
+    }
+}
---
+++ UsbKeyTester/TestUsbKeyForm.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace UsbKeyTester
+{
+    public partial class TestUsbKeyForm : Form
+    {
+        private readonly BackgroundWorker _backgroundWorker = new BackgroundWorker();
+        private static Random _rand = new Random(Guid.NewGuid().GetHashCode());
+        private char[] lettersAndNumbers = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890".ToCharArray();
+        private char[] originalChars;
+        private int linesGenerated = 0;
+        public TestUsbKeyForm()
+        {
+            InitializeComponent();
+            _backgroundWorker.DoWork += (sender, args) =>
+            {
+                RunBtn.Enabled = false;
+                var fullPath = ((DriveRunDetails) args.Argument).FullPath;
+                var availableFreeSpace = ((DriveRunDetails) args.Argument).EmptyBytes;
+                BuildFile(fullPath, availableFreeSpace);
+                CheckFile(fullPath);
+                CheckFileProgressBar.Value = 0;
+                CheckFileProgressBar.Maximum = 1;
+                CreateFileProgressBar.Maximum = 1;
+                CreateFileProgressBar.Value = 0;
+                File.Delete(fullPath);
+            };
+
+            _backgroundWorker.RunWorkerCompleted += (sender, args) =>
+            {
+                RunBtn.Enabled = true;
+            };
+
+            this.FormClosing += (sender, args) =>
+            {
+                if (_backgroundWorker.WorkerSupportsCancellation == true)
+                {
+                    // Cancel the asynchronous operation.
+                    _backgroundWorker.CancelAsync();
+                }
+            };
+        }
+
+        private void TestUsbKeyForm_Load(object sender, EventArgs e)
+        {
+            foreach (var ite in DriveInfo.GetDrives().Select(f => f.Name))
+            {
+                this.DriveDropDownBox.Items.Add(ite);
+            }
+
+            DriveDropDownBox.SelectedIndex = 0;
+        }
+
+        private void RunBtn_Click(object sender, EventArgs e)
+        {
+            var drive = DriveInfo.GetDrives().FirstOrDefault(f => f.Name == DriveDropDownBox.SelectedItem.ToString());
+            var fileName = $"{Guid.NewGuid():N}.img";
+            var fullPath = $"{drive}{fileName}";
+            _backgroundWorker.RunWorkerAsync(new DriveRunDetails {FullPath = fullPath, EmptyBytes = drive.AvailableFreeSpace});
+        }
+
+        private class DriveRunDetails
+        {
+            public string FullPath { get; set; }
+            public long EmptyBytes { get; set; }
+        }
+
+        private void BuildFile(string filePath, long sizeOfUsbInBytes)
+        {
+            const int LenghtOfBytes = 10000000;
+            // as we know in asci a letter is a byte lets make a list 
+            var chars = new List<char>(LenghtOfBytes);
+            for (int i = 0; i < LenghtOfBytes; i++)
+            {
+                chars.Add(lettersAndNumbers[_rand.Next(1, lettersAndNumbers.Length - 1)]);
+            }
+
+            originalChars = chars.ToArray();
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            using (var sw = new StreamWriter(filePath))
+            {
+                for (long i = 0; i < sizeOfUsbInBytes -(LenghtOfBytes+1); i += LenghtOfBytes + 1)
+                {
+                    sw.WriteLine(chars.ToArray());
+                    char first = chars[0];
+                    chars = chars.Skip(1).ToList();
+                    chars.Add(first);
+                    linesGenerated++;
+                    CreateFileProgressBar.Value = (int) (i / LenghtOfBytes);
+                    CreateFileProgressBar.Maximum = (int) (sizeOfUsbInBytes / LenghtOfBytes);
+                }
+            }
+
+            CreateFileProgressBar.Value = 1;
+            CreateFileProgressBar.Maximum = 1;
+        }
+
+        private void CheckFile(string filePath)
+        {
+            var lines = 0;
+            var listChars = originalChars.ToList();
+            using (var fs2 = new StreamReader(filePath))
+            {
+                string fileOneLine = null;
+                while ((fileOneLine = fs2.ReadLine()) != null)
+                {
+                    if (fileOneLine != string.Join("", listChars))
+                    {
+                        MessageBox.Show("miss match in files");
+                        return;
+                    }
+
+                    char first = listChars[0];
+                    listChars = listChars.Skip(1).ToList();
+                    listChars.Add(first);
+                    lines++;
+
+                    CheckFileProgressBar.Maximum = linesGenerated;
+                    CheckFileProgressBar.Value = lines;
+                }
+
+
+                if (linesGenerated != lines)
+                {
+                    MessageBox.Show("miss match in files");
+                    return;
+                }
+
+                MessageBox.Show("Done");
+            }
+        }
+    }
+}
